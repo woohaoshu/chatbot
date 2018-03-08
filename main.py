324895941bf7d01acc4bf814e08c60dc6da7d7f3
woohaoshu <woohaoshu@gmail.com>
2018-03-08 10:24:09 +0800

    增加车龄纠错

--- main.py
+++ main.py
@@ -35,14 +35,16 @@ else:
 print db
 print sessionId
 
+#设置大脑
+if os.path.isfile("bot_brain.brn"):
+    kernel.bootstrap(brainFile = "bot_brain.brn")
+else:
+    kernel.bootstrap(learnFiles = os.path.abspath("aiml/std-startup.xml"), commands = "load aiml b")
+    kernel.saveBrain("bot_brain.brn")
+
 @app.route("/")
 def hello():
-    #设置大脑
-    if os.path.isfile("bot_brain.brn"):
-        kernel.bootstrap(brainFile = "bot_brain.brn")
-    else:
-        kernel.bootstrap(learnFiles = os.path.abspath("aiml/std-startup.xml"), commands = "load aiml b")
-        kernel.saveBrain("bot_brain.brn")
+
     return render_template('chat.html')
 
 
@@ -66,26 +68,29 @@ def ask():
             kernel.resetBrain()
         else:
             bot_response = kernel.respond(message, sessionId)
+
+            try:
+                age = kernel.getPredicate(name="age", sessionID=sessionId)
+                driveyear = kernel.getPredicate(name="driveyear", sessionID=sessionId)
+                print age
+                print driveyear
+                if int(age)<int(driveyear):
+                    kernel.setPredicate(name="driveyear", value=0, sessionID=sessionId)
+                    print kernel.getPredicate(name="driveyear", sessionID=sessionId)
+                    bot_response = kernel.respond("ASK DRIVE YEAR2", sessionId)
+            except ValueError:pass
+
             db.sync()
             return jsonify({'status':'OK','answer':bot_response})
         message = ''
 
 @app.route("/model", methods=['GET'])
 def model():
-    try:
-        name = kernel.getPredicate(name="name",sessionID=sessionId)
-        print name
-    except KeyError:pass
-    try:
-        age = kernel.getPredicate(name="age", sessionID=sessionId)
-    except KeyError:pass
-    try:
-        car = kernel.getPredicate(name="car", sessionID=sessionId)
-    except KeyError:pass
-    try:
-        driveyear = kernel.getPredicate(name="driveyear", sessionID=sessionId)
-    except KeyError:pass
-    data = jsonify({"name":name},{"age":age},{"car":car},{"driveyear":driveyear})
+    name = kernel.getPredicate(name="name",sessionID=sessionId)
+    age = kernel.getPredicate(name="age", sessionID=sessionId)
+    car = kernel.getPredicate(name="car", sessionID=sessionId)
+    driveyear = kernel.getPredicate(name="driveyear", sessionID=sessionId)
+    data = jsonify({"name":name,"age":age,"car":car,"driveyear":driveyear})
     return data
 
 if __name__ == "__main__":
